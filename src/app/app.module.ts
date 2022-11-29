4cd7a09acbdcdcbca0ee2c4ce7369f39cfb70b45
Jack Hulen <111541579+Jhulen37@users.noreply.github.com>
2022-11-28 20:03:32 -0600

    Initial group commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { ServicesComponent } from './services/services.component';
+import { DestinationComponent } from './destination/destination.component';
+import { HotelsComponent } from './hotels/hotels.component';
+import { FlightsComponent } from './flights/flights.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    RegisterPageComponent,
+    ServicesComponent,
+    DestinationComponent,
+    HotelsComponent,
+    FlightsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
