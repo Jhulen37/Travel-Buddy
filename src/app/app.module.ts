b306fd214b878d3ea2a1e654f2ac09b35e37bc5f
Jack Hulen <111541579+Jhulen37@users.noreply.github.com>
2022-12-12 18:46:19 -0600

    home screen/nav updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -13,6 +14,7 @@ import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MaterialModule } from './shared/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
@@ -34,6 +36,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatGridListModule,
+    CommonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('flipState', [
+      state(
+        'active',
+        style({
+          transform: 'rotateY(179deg)',
+        })
+      ),
+      state(
+        'inactive',
+        style({
+          transform: 'rotateY(0)',
+        })
+      ),
+      transition('active => inactive', animate('500ms ease-out')),
+      transition('inactive => active', animate('500ms ease-in')),
+    ]),
+  ],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit(): void {
+  flip: string = 'inactive';
+
+  toggleFlip() {
+    this.flip = this.flip == 'inactive' ? 'active' : 'inactive';
   }
-
 }
